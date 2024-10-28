265eac20f2dea3f6b0a28d2774796e07040f1b53
gastonsantos <gastons525@gmail.com>
2024-10-28 14:12:49 -0300

    cors

--- src/main/java/com/api/pintureria/Pintureria/config/CorsConfig.java
+++ src/main/java/com/api/pintureria/Pintureria/config/CorsConfig.java
@@ -8,21 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
  
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Aplica CORS a todas las rutas
-        .allowedOrigins(
-        	    "http://localhost:3000",
-        	    "https://pintureria-front.vercel.app",
-        	    "https://pintureria-front-b42wsuwca-gastonsantos-projects.vercel.app",
-        	    "https://pintureria-front-olutiwsy9-gastonsantos-projects.vercel.app"
-        	)
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+	    registry.addMapping("/**")
+	            .allowedOrigins(
+	                "http://localhost:3000",
+	                "https://pintureria-front.vercel.app",
+	                "https://pintureria-front-b42wsuwca-gastonsantos-projects.vercel.app",
+	                "https://pintureria-front-olutiwsy9-gastonsantos-projects.vercel.app"
+	            )
+	            .allowedMethods("GET", "POST", "PUT", "DELETE")
+	            .allowedHeaders("Content-Type", "Authorization", "Access-Control-Allow-Origin")
+	            .allowCredentials(true);
+	}
 
-                //.allowedOrigins("https://pintureria-front.vercel.app","https://pintureria-front-h4nzs4zib-gastonsantos-projects.vercel.app")   
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
-                .allowedHeaders("Content-Type", "Authorization")
-
-                //.allowedHeaders("*") // Headers permitidos
-                .allowCredentials(true); // Permitir credenciales (cookies, auth headers)
-    } 
 }
